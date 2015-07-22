ebcf9cdc68202012cc7ec0d53a5362f903c5cc87
Piellia Vasyl <pellia@ukr.net>
2015-07-23 00:21:05 +0300

    Templates

--- app/views.py
+++ app/views.py
@@ -1,8 +1,25 @@
 __author__ = 'Piellia Vasyl'
 
 from app import app
+from flask import render_template, url_for
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Vasyl'}  # fake user
+    return render_template("index.html",
+                           title='Home',
+                           user=user)
+
+
+#@app.route('/books')
+#def show_books():
+#    books = db.session.query(Book)
+#    return render_template('books.html', books=books)
+
+
+#@app.route('/authors')
+#def show_authros():
+#    authors = db.session.query(Author)
+#    return render_template('authors.html', authors=authors)
